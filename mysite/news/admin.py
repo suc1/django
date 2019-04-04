de1e28d908e68693d14236f52ad50daf4268a393
cheng <csu16@ford.com>
2019-04-04 16:25:13 -0400

    2.admin

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Reporter, Article
+
+class ArticleInline(admin.TabularInline):
+    model = Article
+    extra = 3
+    
+class ReporterAdmin(admin.ModelAdmin):
+    inlines = [ArticleInline]
+    list_filter = ['full_name']
+    search_fields = ['full_name']
+    
+admin.site.register(Reporter, ReporterAdmin)
