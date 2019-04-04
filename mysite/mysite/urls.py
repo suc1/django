ca2a8622966d7b8f4de9a90eeeeebbb6bd57f2d1
cheng <csu16@ford.com>
2019-04-04 15:20:59 -0400

    1. Design your model

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
 ]
