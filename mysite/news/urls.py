fab43aa8592ed42fce02d08016a0e721137e1448
cheng <csu16@ford.com>
2019-04-05 12:34:34 -0400

    3. Design your URLs: URLconf

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
+    path('articles/<int:year>/', views.year_archive),
+    path('articles/<int:year>/<int:month>/', views.month_archive),
+    path('articles/detail/<int:pk>/', views.article_detail),
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Article
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the news index.")
+    return render(request, 'news/index.html')
 
+def year_archive(request, year):
+    a_list = Article.objects.filter(pub_date__year=year)
+    context = {'year': year, 'article_list': a_list}
+    return render(request, 'news/year_archive.html', context)
+
+def month_archive(request, year, month):
+    a_list = Article.objects.filter(pub_date__year=year, pub_date__month=month)
+    context = {'year': year, 'month':month, 'article_list': a_list}
+    return render(request, 'news/year_month_archive.html', context)
+
+#every day a pk???
+def article_detail(request, pk):
+    a_list = Article.objects.get(id=pk)
+    context = {'article': a_list}
+    return render(request, 'news/detail_archive.html', context)
